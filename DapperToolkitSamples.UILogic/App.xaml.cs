d85563b0eb1dbb549206b0cd92d34d618b813e3f
Francisco Aguilera <fraguile@microsoft.com>
2017-01-06 21:09:05 -0500

    Implemented prism bootstrapper.

---
+++ DapperToolkitSamples.UILogic/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Copyright (c) Dapper Apps.  All rights reserved.
+ * Use of this sample source code is subject to the terms of the Dapper Apps license 
+ * agreement under which you licensed this sample source code and is provided AS-IS.
+ * If you did not accept the terms of the license agreement, you are not authorized 
+ * to use this sample source code.  For the terms of the license, please see the 
+ * license agreement between you and Dapper Apps.
+ *
+ * To see the article about this app, visit http://www.dapper-apps.com/DapperToolkit
+ */
+
+using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+using DapperToolkitSamples.Views;
+using Microsoft.Practices.Prism.Mvvm;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+namespace DapperToolkitSamples
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public sealed partial class App : MvvmAppBase
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate("Main", null);
+            return Task.FromResult<object>(null);
+        }
+    }
+}
